2bb86a87b8ed650e18ef42cbf9d39a78e7e1e659
girlanesl <girla10neslima@gmail.com>
2026-07-27 23:12:02 -0300

    feat: move login to auth/login route

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,9 @@
-import LoginComponent from "./components/LoginComponent";
+
 
 export default function Home() {
   return (
     <div className="bg-[#F6F6F2] w-screen h-screen">
-      <LoginComponent />
+      {/* <LoginComponent /> */}
     </div>
   );
 }
